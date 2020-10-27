8707b9e3f8b83d4ec5b037129b30c40a534845d4
katerynakofanova <katerynakofanova@gmail.com>
2020-10-27 15:55:18 +0200

    update readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+const App = () => {
+  const [display, setDisplay] = useState('0')
+
+  const handleChange = (value) => {
+    if (display === '0'){
+      switch(value){
+        case '+':
+          setDisplay(display+value)
+          break
+        case '-':
+          setDisplay(display+value)
+          break
+        case '*':
+          setDisplay(display+value)
+          break
+        case '/':
+          setDisplay(display+value)
+          break
+        case '.':
+          setDisplay(display+value)
+          break
+        case '0':
+          setDisplay(value)
+          break
+        default:
+          setDisplay(value)
+      }
+    }else{
+      setDisplay(display+value)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="calculator">
+        <div className="calc_display">
+          {display}
+        </div>
+          <div className="calc_keys">
+            <button className="operator plus" onClick={() => handleChange('+')}>+</button>
+            <button className="operator minus" onClick={() => handleChange('-')}>-</button>
+            <button className="operator times" onClick={() => handleChange('*')}>&times;</button>
+            <button className="operator divide" onClick={() => handleChange('/')}>÷</button>
+            <button className="number one" onClick={() => handleChange('1')}>1</button>
+            <button className="number two" onClick={() => handleChange('2')}>2</button>
+            <button className="number three" onClick={() => handleChange('3')}>3</button>
+            <button className="number four" onClick={() => handleChange('4')}>4</button>
+            <button className="number five" onClick={() => handleChange('5')}>5</button>
+            <button className="number six" onClick={() => handleChange('6')}>6</button>
+            <button className="number seven" onClick={() => handleChange('7')}>7</button>
+            <button className="number eight" onClick={() => handleChange('8')}>8</button>
+            <button className="number nine" onClick={() => handleChange('9')}>9</button>
+            <button className="number zero" onClick={() => handleChange('0')}>0</button>
+            <button className="number decimal" onClick={() => handleChange('.')}>.</button>
+            <button className="number clear" onClick={() => setDisplay('0')}>AC</button>
+            <button className="equal"onClick={() => setDisplay(eval(display).toString())}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
